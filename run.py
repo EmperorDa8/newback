d95b8b10a4e50950d49dee4e3727ad419dde0f6c
Dan Usman <86113954+EmperorDa8@users.noreply.github.com>
2022-03-27 13:00:26 -0700

    Update run.py

--- run.py
+++ run.py
@@ -7,14 +7,14 @@ import json
 
 path="/supplier_data/descriptions/"
 full_path=os.getcwd()+path
-
+url="http://localhost/fruits"
 def upload_desc():
   for item in os.listdir(full_path):
     if item.endswith(".txt"):
       fruit_name=os.path.splitext(item)[0]
       with open(full_path+item,"r") as f:
         data=f.read().split("\n")
-        fruit_data= {"name":data[0], "weight":data[1].strip(" lbs"), "description":data[2], "image_name":fruit_name+(".jpeg")
+        fruit_data= {"name":data[0], "weight":data[1].strip(" lbs"), "description":data[2], "image_name":fruit_name+".jpeg"}
         r = requests.post(url, json=fruit_data)
         print (r.status_code)
         print ("upload success")
